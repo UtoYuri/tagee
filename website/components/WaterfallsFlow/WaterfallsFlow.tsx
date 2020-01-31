8e771af1a3bfe36d4abd0fba148dcb81e91ea14e
‘yuri’ <yuri@utohub.com>
2020-02-01 03:10:19 +0800

    add waterfallsflow

---
+++ website/components/WaterfallsFlow/WaterfallsFlow.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import _ from 'lodash';
+
+import styles from './WaterfallsFlow.less';
+
+interface Props {
+  columns?: number;
+  gutter?: number;
+  children: JSX.Element[];
+}
+
+const WaterfallsFlow = function(props: Props) {
+  const { columns, gutter, children } = props;
+
+  return (
+    <div className={styles.waterfalls} style={{ columnCount: columns || 5, columnGap: `${gutter || 10}px` }}>
+      {
+        children.map((element: JSX.Element, index: number) => (
+          <div className={styles.item} key={`item-${index}`}>{element}</div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default WaterfallsFlow;
